903a0189dbe4039d8db4fbb0d85c2c433665d45b
EdwinHu-OvO <1721877830@qq.com>
2024-05-17 10:00:01 +0800

    test commit function

--- 各种临时尝试/源.cpp
+++ 各种临时尝试/源.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
-int main()
-{
-	int a = 1;
-	const int * const ip = &a;
-	int * ip2 = (int *)ip;
-	*ip2 = 2;
-	//*ip = 1;
-	return 0;
+
+static char* f1(int c){
+	char a[] = "augidfaiwuh";
+	char b[] = "avbduiabi";
+	char* m;
+	if (c < 6) {
+		m = a;
+		//cout << m << endl;
+	}
+	else {
+		m = b;
+		cout << m << endl;
+	}
+	return m;
+}
+
+void main() {
+	cout << f1(8);
 }
